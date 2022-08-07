785806a6551edf760f0b47abba00e6335abbac86
Erik Larsson <erik.mattias.larsson@gmail.com>
2022-08-07 09:37:55 +0200

    New frames and some animation code

--- src/animate-sprite.js
+++ src/animate-sprite.js
@@ -4,17 +4,35 @@ function animateSprite(sprite, name, amount) {
 	frame.position.x = 20
 
 	let delay = 0
+    let pause = false
 	const frameDurationModifier = 0.1
 	const FPS = 60 // assume this
 	const MOD = FPS * frameDurationModifier
 
     addTicker('game-stuff', () => {
+        if (pause) return
     	delay += gameSpeed
     	if (delay >= MOD * amount) delay = 0
     	const currentFrame = Math.max(1, Math.ceil(delay / MOD))
     	frame.texture = Texture.fromImage(`images/animation-test/${name}/${currentFrame}.png`)
-    	// console.log(delay)
     })
 
     sprite.addChild(frame)
+
+    const obj = {
+        sprite: frame,
+        restart: () => {
+            delay = 0
+            return obj
+        },
+        start: () => {
+            pause = false
+            return obj
+        },
+        pause: () => {
+            pause = true
+            return obj
+        }
+    }
+    return obj
 }
