2f413fac361d0fbd9b04eb08abc9b0ade77b00ce
Erik Larsson <erik.mattias.larsson@gmail.com>
2022-10-05 12:52:39 +0200

    Animating splash screen with bezier curvers

--- src/setup-splash.js
+++ src/setup-splash.js
@@ -13,31 +13,80 @@ function setupSplash() {
   splashscreen.drawRect(0, 0, WIDTH, HEIGHT)
   container.addChild(splashscreen)
 
+  const text = new Container()
+  container.addChild(text)
+
   const title = new PIXI.Text('Bee Game', { ...fontConfig, ...hugeFont, fill: '#c96f10' })
   title.anchor.set(0.5, 0)
   title.position.x = Math.round(WIDTH / 2 / 2) + 8
   title.position.y = 30
-  container.addChild(title)
+  text.addChild(title)
 
   const catchphrase = new PIXI.Text('No bee puns guaranteed', { ...fontConfig, ...massiveFont, fill: 'black' })
   catchphrase.anchor.set(0.5, 0)
   catchphrase.position.x = Math.round(WIDTH / 2 / 2)
   catchphrase.position.y = 110
-  container.addChild(catchphrase)
+  text.addChild(catchphrase)
 
-  const welcomeBee = Sprite.fromImage('images/bee/bee-drone-reference.png')
+  const welcomeFlapA = Texture.fromImage('images/bee/bee-drone-flap.png')
+  const welcomeFlapB = Texture.fromImage('images/bee/bee-drone-flop.png')
+  const welcomeFlapC = Texture.fromImage('images/bee/bee-drone-reference.png')
+  const welcomeBee = new Sprite(welcomeFlapA)
   welcomeBee.scale.x = 2
   welcomeBee.scale.y = 2
-  welcomeBee.position.x = Math.round(WIDTH / 2 / 2) - 25
-  welcomeBee.position.y = Math.round(HEIGHT / 2 / 2) - 0
-  splashscreen.addChild(welcomeBee)
+  welcomeBee.position.x = 0
+  welcomeBee.position.y = 0
+  container.addChild(welcomeBee)
+
+  const targetX = Math.round(WIDTH / 2 / 2) - 25
+  const targetY = Math.round(HEIGHT / 2 / 2) - 7
+  const points = [
+    [-50, -50],
+    [10, 90],
+    [110, 100],
+    [targetX, targetY],
+  ]
+  points.forEach(point => {
+    return
+    const [x, y] = point
+    const p = new Graphics()
+    p.beginFill(0xff0000)
+    p.drawRect(x, y, 2, 2)
+    container.addChild(p)
+  })
+  const beeBezier = new Bezier(
+    ...points.flatMap(p => p)
+  )
+
+  const LUT_MAX = 250
+
+  const fadeout = new Bezier({ x: 0, y: 0 }, { x: 0.12, y: 0.78 }, { x: 0, y: 1 }, { x: 1, y: 1 })
+  const fadeoutLUT = fadeout.getLUT(LUT_MAX)
+
+  const luts = beeBezier.getLUT(LUT_MAX)
+  
+  let counter = 0
+
+  const interval = setInterval(() => {
+    const target = Math.round(fadeoutLUT[counter].y * LUT_MAX)
+    welcomeBee.position.x = luts[target].x
+    welcomeBee.position.y = luts[target].y
+    counter++
+
+    welcomeBee.texture = counter % 10 < 5 ? welcomeFlapA : welcomeFlapB
+    
+    if (counter >= LUT_MAX) {
+      clearInterval(interval)
+      welcomeBee.texture = welcomeFlapC
+    }
+  }, 16.66)
 
   const welcomeHoney = Sprite.fromImage('images/hex/honey/cell-honey-full.png')
   welcomeHoney.scale.x = 2
   welcomeHoney.scale.y = 2
   welcomeHoney.position.x = Math.round(WIDTH / 2 / 2) + 5
   welcomeHoney.position.y = Math.round(HEIGHT / 2 / 2) - 1
-  splashscreen.addChild(welcomeHoney)
+  container.addChild(welcomeHoney)
 
   const callbackA = () => {
     app.stage.removeChild(container)
