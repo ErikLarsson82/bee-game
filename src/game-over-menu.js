de63e52f73b4ac9696548e2ee11b28d74de7ae02
Erik Larsson <erik.mattias.larsson@gmail.com>
2023-02-20 15:14:56 +0100

    The ability to keep playing

--- src/game-over-menu.js
+++ src/game-over-menu.js
@@ -3,6 +3,7 @@ const DEBUG_BUTTON = false
 function createGameOverUI() {
   const background = Sprite.fromImage('images/ui/game-over-background-failed.png')
   const successTexture = Texture.fromImage('images/ui/game-over-background-success.png')
+  const failTexture = Texture.fromImage('images/ui/game-over-background-failed.png')
   background.position.x = 146
   background.position.y = 98
   background.visible = false
@@ -18,29 +19,34 @@ function createGameOverUI() {
     )
   }
 
-  background.addChild(
-  	Button(5, 90, 'Main Menu', () => {
-      app.stage.removeChild(container)
-      app.ticker.remove(gameloop)
-      setupWorldMap3()
-    })
-  )
+  const buttonA = Button(5, 90, 'Main Menu', () => {
+    app.stage.removeChild(container)
+    app.ticker.remove(gameloop)
+    setupWorldMap3()
+  })
+  background.addChild(buttonA)
 
-  background.addChild(
-    Button(66, 90, 'Continue', () => {
-      keepPlaying = true
-      gameover = false
-      background.visible = false
-    })
-  )
+  const buttonB = Button(66, 90, 'Continue', () => {
+    keepPlaying = true
+    gameover = false
+    background.visible = false
+  })
+  background.addChild(buttonB)
 
   addTicker('ui', () => {
     if (gameover) {
-    	background.visible = true
+      background.visible = true
 
-      if (levelCompleteCriteria(currentCycleIndex)) {
+      if (levelCompleteCriteria(currentCycleIndex) && !keepPlaying) {
+        buttonA.position.x = 5
+        buttonB.visible = true
+
         saveProgress(levelIndex, year)
         background.texture = successTexture
+      } else {
+        buttonA.position.x = 36
+        buttonB.visible = false
+        background.texture = failTexture
       }
     }
   })
