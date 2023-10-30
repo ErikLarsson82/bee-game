d6de2720effc81a0a7db96cdc1b4c5f5c7227286
Erik Larsson <erik.mattias.larsson@gmail.com>
2023-10-30 16:04:44 +0100

    preloading images

--- src/main.js
+++ src/main.js
@@ -3,18 +3,6 @@ const version = '0.5.1'
 
 let DEBUG = false
 
-// Works on PC =  "PICO-8 mono Regular"'
-// Works on MAC = 'beefont'
-const fontConfig = {
-    fontFamily: 'beefont',
-    fill: 'white'
-}
-
-const smallFont = { fontSize: 4 }
-const largeFont = { fontSize: 8 }
-const massiveFont = { fontSize: 16 }
-const hugeFont = { fontSize: 50 }
-
 const colors = {
   yellow: '#fee761',
   orange: '#feae34',
@@ -98,36 +86,52 @@ let angels = []
 let hoveredCells = []
 
 loader.add('ironchest-bee.ttf')
+loader.load(step2)
 
-loader.add('bee-working-animation-worker', 'images/sprite-sheets/bee/worker/working.png')
-loader.add('bee-eating-animation-worker', 'images/sprite-sheets/bee/worker/eating.png')
-loader.add('bee-unloading-animation-worker', 'images/sprite-sheets/bee/worker/unloading.png')
-loader.add('bee-dying-age-animation-worker', 'images/sprite-sheets/bee/worker/dying-age.png')
-loader.add('bee-dying-hunger-animation-worker', 'images/sprite-sheets/bee/worker/dying-hunger.png')
+function step2() {
+  loader.on('progress', function(e) {
+    loadingLabel.text = `Loading ${e.progress.toFixed(0)} %`
+  })
+  
+  // shorthands
+  loader.add('bee-working-animation-worker', 'images/sprite-sheets/bee/worker/working.png')
+  loader.add('bee-eating-animation-worker', 'images/sprite-sheets/bee/worker/eating.png')
+  loader.add('bee-unloading-animation-worker', 'images/sprite-sheets/bee/worker/unloading.png')
+  loader.add('bee-dying-age-animation-worker', 'images/sprite-sheets/bee/worker/dying-age.png')
+  loader.add('bee-dying-hunger-animation-worker', 'images/sprite-sheets/bee/worker/dying-hunger.png')
 
-loader.add('bee-working-animation-nurser', 'images/sprite-sheets/bee/nurser/working.png')
-loader.add('bee-eating-animation-nurser', 'images/sprite-sheets/bee/nurser/eating.png')
-loader.add('bee-unloading-animation-nurser', 'images/sprite-sheets/bee/nurser/unloading.png')
-loader.add('bee-dying-age-animation-nurser', 'images/sprite-sheets/bee/nurser/dying-age.png')
-loader.add('bee-dying-hunger-animation-nurser', 'images/sprite-sheets/bee/nurser/dying-hunger.png')
+  loader.add('bee-working-animation-nurser', 'images/sprite-sheets/bee/nurser/working.png')
+  loader.add('bee-eating-animation-nurser', 'images/sprite-sheets/bee/nurser/eating.png')
+  loader.add('bee-unloading-animation-nurser', 'images/sprite-sheets/bee/nurser/unloading.png')
+  loader.add('bee-dying-age-animation-nurser', 'images/sprite-sheets/bee/nurser/dying-age.png')
+  loader.add('bee-dying-hunger-animation-nurser', 'images/sprite-sheets/bee/nurser/dying-hunger.png')
 
-loader.add('bee-working-animation-forager', 'images/sprite-sheets/bee/forager/working.png')
-loader.add('bee-eating-animation-forager', 'images/sprite-sheets/bee/forager/eating.png')
-loader.add('bee-unloading-animation-forager', 'images/sprite-sheets/bee/forager/unloading.png')
-loader.add('bee-dying-age-animation-forager', 'images/sprite-sheets/bee/forager/dying-age.png')
-loader.add('bee-dying-hunger-animation-forager', 'images/sprite-sheets/bee/forager/dying-hunger.png')
+  loader.add('bee-working-animation-forager', 'images/sprite-sheets/bee/forager/working.png')
+  loader.add('bee-eating-animation-forager', 'images/sprite-sheets/bee/forager/eating.png')
+  loader.add('bee-unloading-animation-forager', 'images/sprite-sheets/bee/forager/unloading.png')
+  loader.add('bee-dying-age-animation-forager', 'images/sprite-sheets/bee/forager/dying-age.png')
+  loader.add('bee-dying-hunger-animation-forager', 'images/sprite-sheets/bee/forager/dying-hunger.png')
 
-loader.add('bee-working-animation-idle', 'images/sprite-sheets/bee/idle/working.png')
-loader.add('bee-eating-animation-idle', 'images/sprite-sheets/bee/idle/eating.png')
-loader.add('bee-unloading-animation-idle', 'images/sprite-sheets/bee/idle/unloading.png')
-loader.add('bee-dying-age-animation-idle', 'images/sprite-sheets/bee/idle/dying-age.png')
-loader.add('bee-dying-hunger-animation-idle', 'images/sprite-sheets/bee/idle/dying-hunger.png')
+  loader.add('bee-working-animation-idle', 'images/sprite-sheets/bee/idle/working.png')
+  loader.add('bee-eating-animation-idle', 'images/sprite-sheets/bee/idle/eating.png')
+  loader.add('bee-unloading-animation-idle', 'images/sprite-sheets/bee/idle/unloading.png')
+  loader.add('bee-dying-age-animation-idle', 'images/sprite-sheets/bee/idle/dying-age.png')
+  loader.add('bee-dying-hunger-animation-idle', 'images/sprite-sheets/bee/idle/dying-hunger.png')
 
-loader.add('bee-converting-nectar-animation-worker', 'images/sprite-sheets/bee/worker/converting-nectar.png')
+  loader.add('bee-converting-nectar-animation-worker', 'images/sprite-sheets/bee/worker/converting-nectar.png')
 
-loader.add('hex-hatching', 'images/sprite-sheets/hex/hatching.png')
+  loader.add('hex-hatching', 'images/sprite-sheets/hex/hatching.png')
 
-loader.add('flower-is-pollinated', 'images/sprite-sheets/flower-is-pollinated/animation-spritesheet-flower.png')
+  loader.add('flower-is-pollinated', 'images/sprite-sheets/flower-is-pollinated/animation-spritesheet-flower.png')
+  
+  // preload menu only
+  loader.add('images/ui/button-huge/button-huge-standard.png')
+  loader.add('images/ui/button-huge/button-huge-hover.png')
+  loader.add('images/ui/button-huge/button-huge-click.png')
 
-loader.load(setupDebugMenu)  
-// loader.load(setupSplash)
+  loader.load(() => {
+    app.stage.removeChild(loadingContainer)
+    setupDebugMenu()
+    // loader.load(setupSplash)
+  })
+}
--- src/setup-pixi.js
+++ src/setup-pixi.js
@@ -10,3 +10,25 @@ settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST // Pixel-scaling
 
 app.renderer.view.style.position = 'absolute'
 app.renderer.view.style.display = 'block'
+
+// Works on PC =  "PICO-8 mono Regular"'
+// Works on MAC = 'beefont'
+const fontConfig = {
+    fontFamily: 'beefont',
+    fill: 'white'
+}
+
+const smallFont = { fontSize: 4 }
+const largeFont = { fontSize: 8 }
+const massiveFont = { fontSize: 16 }
+const hugeFont = { fontSize: 50 }
+
+const loadingContainer = new Container()
+loadingContainer.scale.x = 2
+loadingContainer.scale.y = 2
+app.stage.addChild(loadingContainer)
+
+const loadingLabel = new PIXI.Text('Loading', { fontFamily: 'verdana', fill: 'white', fontSize: 10 })
+loadingLabel.position.x = 170
+loadingLabel.position.y = 140
+loadingContainer.addChild(loadingLabel)
